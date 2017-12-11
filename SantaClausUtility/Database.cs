7215fcdd100a7187b5e47c68eb6dba1d65fb609f
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-12-11 17:51:49 +0100

    Generic DB Class

--- SantaClausUtility/Database.cs
+++ SantaClausUtility/Database.cs
@@ -1,21 +1,65 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SantaClausUtility
 {
-    public sealed class Database
+    public sealed class Database<T> : IConnect<Database<T>.DatabaseSettings, T>, ICRUD<Database<T>, Database<T>.DatabaseSettings>
     {
-        private static readonly Database _instance = new Database();
+        #region Singleton Declaration
 
-        private Database() {}
+        private static readonly Database<T> _instance = new Database<T>();
 
-        public static Database Instance
+        public static Database<T> Instance { get; }
+
+        private Database()
         {
-            get
-            {
-                return _instance;
-            }
+            _connectionInstance = Connect(); //TODO
+        }
+
+        #endregion  
+
+        private class DatabaseSettings
+        {
+            public string DatabaseUser { get; set; }
+            public string DatabaseUserPassword { get; set; }
+            public string DatabaseName { get; set; }
+            public string DatabaseHost { get; set; }
+            public string DatabaseHostPort { get; set; }
+        }
+
+        private DatabaseSettings Settings { get; set; }
+        private T _connectionInstance;
+
+        Database<T> ICRUD<Database<T>, DatabaseSettings>.Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICRUD<Database<T>, DatabaseSettings>.Create(Database<T> element)
+        {
+            throw new NotImplementedException();
+        }
+
+        Database<T> ICRUD<Database<T>, DatabaseSettings>.Read(DatabaseSettings id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICRUD<Database<T>, DatabaseSettings>.Update(Database<T> element)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICRUD<Database<T>, DatabaseSettings>.Delete(DatabaseSettings id)
+        {
+            throw new NotImplementedException();
+        }
+
+        T IConnect<DatabaseSettings, T>.Connect(Func<DatabaseSettings, T> connection)
+        {
+            return connection(Settings);
         }
     }
 }
---
+++ SantaClausUtility/IConnect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantaClausUtility
+{
+    public interface IConnect<TypeHostSettings, TypeHostInstance>
+    {
+        TypeHostInstance Connect(Func<TypeHostSettings, TypeHostInstance> connection);
+    }
+}
